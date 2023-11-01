bb3f9367d45534bc68b2508cb89c9c8edb1cf1c7
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2023-11-01 23:25:33 +0100

    feat: Add CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
+pub mod cli;
+mod file;
+pub mod generate;
 pub mod tree_generator;
+pub mod update;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use project_tree_manager::cli::cli;
+use project_tree_manager::generate::generate;
+use project_tree_manager::update::update;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("generate", sub_matches)) => {
+            let config = &sub_matches
+                .value_of_t::<String>("config")
+                .unwrap_or_else(|e| e.exit());
+            let folder = &sub_matches
+                .value_of_t::<String>("folder")
+                .unwrap_or_else(|e| e.exit());
+            generate(config, folder)
+        }
+        Some(("update", _)) => {
+            update()
+        }
+        _ => unreachable!(),
+    }
 }
