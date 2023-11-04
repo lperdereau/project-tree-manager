8232c0efffde065a8444b64dde5572ff8be9f765
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2023-11-04 01:06:44 +0100

    fix: Add checksum to update feature

--- src/update.rs
+++ src/update.rs
@@ -1,5 +1,6 @@
 use reqwest::header;
 use self_update::cargo_crate_version;
+use self_update::update::Release;
 use std::fs::File;
 use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 use std::{env, fs};
@@ -19,19 +20,34 @@ pub fn update(force: bool) {
         return;
     }
 
-    let asset = releases[0].asset_for(&get_target(), Option::None).unwrap();
+    display_changelog(&releases[0]);
+
+    let data = download_asset(&releases[0], Option::None);
+    let sum = get_sum(&releases[0], &get_current_target());
+    if !check_sum(data.as_slice(), sum.unwrap()) {
+        return;
+    }
+
+    let binary = uncompress_binary(data);
+    update_binary_file(binary);
+
+    println!("Up to date ✅");
+}
+
+fn get_current_target() -> String {
+    format!("{}-{}", std::env::consts::ARCH, std::env::consts::OS)
+}
+
+fn download_asset(release: &Release, name: Option<&str>) -> Vec<u8> {
+    let current_target = get_current_target();
+    let target: &str = match name {
+        Some(value) => value,
+        None => current_target.as_str().as_ref(),
+    };
+
+    let asset = release.asset_for(target, Option::None).unwrap();
 
     let mut c = Cursor::new(Vec::new());
-
-    println!("Downloading: {}:{}", asset.name, releases[0].version);
-    termimad::print_text(
-        releases[0]
-            .body
-            .clone()
-            .unwrap()
-            .replace("\n\n", "\n")
-            .as_str(),
-    );
 
     self_update::Download::from_url(&asset.download_url)
         .set_header(
@@ -46,11 +62,17 @@ pub fn update(force: bool) {
     c.seek(SeekFrom::Start(0)).unwrap();
     let mut out = Vec::new();
     c.read_to_end(&mut out).unwrap();
+    out
+}
 
-    let mut decoder = XzDecoder::new(&out[..]);
+fn uncompress_binary(buf: Vec<u8>) -> Vec<u8> {
+    let mut decoder = XzDecoder::new(&buf[..]);
     let mut decompressed_data = Vec::new();
     decoder.read_to_end(&mut decompressed_data).unwrap();
+    decompressed_data
+}
 
+fn update_binary_file(buf: Vec<u8>) {
     let mut output_path = env::current_exe().unwrap();
     let origin = output_path.clone();
 
@@ -67,21 +89,46 @@ pub fn update(force: bool) {
 
     let mut output_file = File::create(&output_path).unwrap();
     output_file
-        .write_all(decompressed_data.as_slice())
+        .write_all(buf.as_slice())
         .expect("Failed to write new version of binary.");
 
-    output_file.set_permissions(
-        File::open(&origin)
-            .unwrap()
-            .metadata()
-            .unwrap()
-            .permissions(),
-    ).unwrap();
+    output_file
+        .set_permissions(
+            File::open(&origin)
+                .unwrap()
+                .metadata()
+                .unwrap()
+                .permissions(),
+        )
+        .unwrap();
     fs::rename(output_path, origin).unwrap();
-
-    println!("Up to date ✅");
 }
 
-fn get_target() -> String {
-    format!("{}-{}", std::env::consts::ARCH, std::env::consts::OS)
+fn get_sum(release: &Release, target: &str) -> Option<String> {
+    let checksum = download_asset(release, Option::Some("sha256sum"));
+    for line in std::str::from_utf8(&checksum).unwrap().split('\n') {
+        if line.contains(target) {
+            let sum = line.chars().take_while(|&ch| ch != ' ').collect::<String>();
+            return Option::Some(sum);
+        }
+    }
+    Option::None
+}
+
+fn check_sum(buf: &[u8], sum: String) -> bool {
+    let digest = sha256::digest(buf);
+    if digest != sum {
+        println!("❌ Sums are differents.");
+        println!("");
+        println!("Downloaded: {}", digest);
+        println!("Source: {}", sum);
+        return false;
+    }
+    true
+}
+
+fn display_changelog(release: &Release) {
+    println!("");
+    println!("");
+    termimad::print_text(release.body.clone().unwrap().replace("\n\n", "\n").as_str());
 }
